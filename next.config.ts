0941413f547dd71335ba6a5d93437f6bc29a78f6
Abhishek <abhishekbwathore@gmail.com>
2026-07-21 20:37:08 +0530

    Configure Next.js for static export deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/dental-clinic', // ⚠️ Replace this with your exact repository name
   images: {
+    unoptimized: true, // Required for static exporting
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        hostname: "://googleusercontent.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
